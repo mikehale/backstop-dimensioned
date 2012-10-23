72f2357f16a02d83dcdcd215d44db5cf1a80ce3d
Michael Hale <mike@hales.ws>
2012-10-23 18:07:32 -0400

    stub uri

--- lib/backstop-dimensioned/web.rb
+++ lib/backstop-dimensioned/web.rb
@@ -1,6 +1,9 @@
 module Backstop
   module Dimensioned
     class Web < Sinatra::Base
+      post '/publish/custom/dimensioned' do
+        
+      end
     end
   end
 end
--- spec/backstop-dimensioned/web_spec.rb
+++ spec/backstop-dimensioned/web_spec.rb
@@ -8,5 +8,11 @@ describe Backstop::Dimensioned::Web do
     Backstop::Dimensioned::Web
   end
 
-  
+  describe 'POST /publish/custom/dimensioned' do
+    it('should return a 200') do
+      post "/publish/custom/dimensioned", {}
+      last_response.status.should eq(200)
+    end
+  end
+
 end
