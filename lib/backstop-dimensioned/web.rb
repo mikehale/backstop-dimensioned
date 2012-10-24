70525d88ed35553b8ff689bb0efd41673dfd0ba5
Michael Hale <mike@hales.ws>
2012-10-24 18:15:57 -0400

    simplify the web stuff, need to figure out how to test this better

--- lib/backstop-dimensioned/web.rb
+++ lib/backstop-dimensioned/web.rb
@@ -1,43 +1,80 @@
 require 'json'
 
+module Librato
+  class MetricTooOldError < RuntimeError
+  end
+end
+
 module Backstop
   module Dimensioned
     class Web < Sinatra::Base
 
       helpers do
-        def submit_to_graphite(name, value, measure_time, period)
+        def log(data, &blk)
+          Scrolls.log(data.merge(:app => 'backstop-dimensioned', :ps => 'web'), &blk)
+        end
+        
+        def queue
+          @@queue ||= Librato::Metrics::Queue.new(:autosubmit_interval => 60, :autosubmit_count => 400)
         end
 
-        def submit_to_librato(name, value, source, measure_time, period)
+        def submit_to_graphite(m)
+          dimensions = m["dimensions"] || []
+          name = m["metric"]
+          period = m["period"]
+          measure_time = m["measure_time"]
 
+          %w[sum min max count].each do |value_name|
+            if value = m[value_name]
+              #submit_to_graphite("#{[dimensions, name].join(".")}.#{value_name}", value, measure_time, period)
+            end
+          end
+        end
+
+        def submit_to_librato(m)
+          dimensions = m["dimensions"] || []
+          name = m["metric"]
+          period = m["period"]
+          measure_time = m["measure_time"]
+
+          value = Hash[%w[sum min max count].map do |value_name|
+                         [value_name, m[value_name]]
+                       end]
+
+          source = if dimensions
+                     {:source => dimensions.join(".")}
+                   else
+                     {}
+                   end
+
+
+          begin
+            if Time.at(measure_time.to_i) <= (Time.now - 7140)
+              raise Librato::MetricTooOldError
+            else
+              queue.add(name => { :period => period, :measure_time => measure_time }.merge(value).merge(source) )
+            end
+          rescue => e
+            log(:fn => 'submit_to_librato', :at => 'error', :exception => e.class.name, :metric => name, :value => value.inspect, :measure_time => measure_time)
+          end
         end
 
         def submit(measurements)
           measurements.each do |m|
-            dimensions = m["dimensions"] || []
-            name = m["metric"]
-            period = m["period"]
-            measure_time = m["measure_time"]
-            
-            %w[sum min max count].each do |value_name|
-              if value = m[value_name]
-                submit_to_graphite("#{[dimensions, name].join(".")}.#{value_name}", value, measure_time, period)
-              end
-            end
-
-
-            value = Hash[%w[sum min max count].map do |value_name|
-                           [value_name, m[value_name]]
-                         end]
-            submit_to_librato(name, value, dimensions.join("."), measure_time, period)
+            submit_to_graphite(m)
+            submit_to_librato(m)
           end
-
         end
+
+        def measurements(request)
+          JSON.parse(request.body.read)
+        end
+
       end
 
       post '/publish/custom/dimensioned' do
-        measurements = JSON.parse(request.body.read)
-        submit(measurements)
+        submit(measurements(request))
+
         {}.to_json
       end
     end
--- spec/backstop-dimensioned/web_spec.rb
+++ spec/backstop-dimensioned/web_spec.rb
@@ -19,18 +19,7 @@ describe Backstop::Dimensioned::Web do
     context "multi measured sample" do
       let(:measure_time) { Time.now.to_i }
       let(:data) do
-        [
-         {
-           :metric => "latency",
-           :count => 10,
-           :sum => 100,
-           :min => 10,
-           :max => 11,
-           :measure_time => measure_time,
-           :period => 60,
-           :dimensions => %w[test elb123 us-east-1a]
-         }
-        ]
+        [{}]
       end
 
       it('should return a 200') do
@@ -39,31 +28,14 @@ describe Backstop::Dimensioned::Web do
       end
 
       it "should submit the correct data to graphite" do
-        app.any_instance.should_receive(:submit_to_graphite).with("test.elb123.us-east-1a.latency.count", 10, measure_time, 60)
-        app.any_instance.should_receive(:submit_to_graphite).with("test.elb123.us-east-1a.latency.sum", 100, measure_time, 60)
-        app.any_instance.should_receive(:submit_to_graphite).with("test.elb123.us-east-1a.latency.min", 10, measure_time, 60)
-        app.any_instance.should_receive(:submit_to_graphite).with("test.elb123.us-east-1a.latency.max", 11, measure_time, 60)
-
+        app.any_instance.should_receive(:submit_to_graphite).with(data.first)
         post "/publish/custom/dimensioned", data.to_json
       end
 
       it "should submit the correct data to librato" do
-        app.any_instance.should_receive(:submit_to_librato).with(
-                                                                 "latency",
-                                                                 {
-                                                                   "count" => 10,
-                                                                   "sum" => 100,
-                                                                   "min" => 10,
-                                                                   "max" => 11
-                                                                 },
-                                                                 "test.elb123.us-east-1a",
-                                                                 measure_time,
-                                                                 60)
+        app.any_instance.should_receive(:submit_to_librato).with(data.first)
         post "/publish/custom/dimensioned", data.to_json
       end
-    end
-
-    context "single measured sample" do
     end
   end
 
